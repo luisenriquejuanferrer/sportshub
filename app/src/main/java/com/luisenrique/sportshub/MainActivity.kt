7297deb4468f28634e616bac62219e24fea5279c
Luis Enrique <luijuafer@alu.edu.gva.es>
2025-11-19 16:46:07 +0100

    Añadido topbar y menú de hamburguesa al MainActivity.kt

--- app/src/main/java/com/luisenrique/sportshub/MainActivity.kt
+++ app/src/main/java/com/luisenrique/sportshub/MainActivity.kt
@@ -6,24 +6,50 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.luisenrique.sportshub.ui.components.MainNavigationDrawer
+import com.luisenrique.sportshub.ui.components.TopBar
 import com.luisenrique.sportshub.ui.theme.SportsHubTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val drawerState = rememberDrawerState(DrawerValue.Closed)
+            val scopeDrawer = rememberCoroutineScope()
+
             SportsHubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MainNavigationDrawer(drawerState) {
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            TopBar("Sports Hub - IES Chabàs", drawerState, scopeDrawer)
+                        }
+                    ) { innerPadding ->
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(innerPadding)
+                        )
+                    }
                 }
             }
         }
