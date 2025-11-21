5c0543e29904c9dfd5a2c83d180c580b968c4299
Luis Enrique <luijuafer@alu.edu.gva.es>
2025-11-21 15:43:20 +0100

    MatchesScreen.kt finalizado y item  MatchItem.kt por cada partido.

---
+++ app/src/main/java/com/luisenrique/sportshub/ui/components/MatchItem.kt
@@ -0,0 +1,76 @@
+package com.luisenrique.sportshub.ui.components
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.luisenrique.sportshub.R
+import com.luisenrique.sportshub.ui.screens.Partido
+
+@Composable
+fun MatchItem(partido: Partido) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Column {
+            MyText(text = partido.fecha)
+            Spacer(Modifier.padding(vertical = 8.dp))
+            Row {
+                MyImage(
+                    painter = painterResource(R.drawable.escudo_ies_chabas),
+                    contentDescription = "Logo equipo",
+                    modifier = Modifier
+                        .size(26.dp)
+                        .clip(CircleShape),
+                    alignment = Alignment.CenterStart,
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(Modifier.padding(horizontal = 4.dp))
+                MyText(text = partido.local)
+            }
+            Spacer(Modifier.padding(vertical = 8.dp))
+            Row {
+                MyImage(
+                    painter = painterResource(R.drawable.escudo_maria_ivars),
+                    contentDescription = "Logo equipo",
+                    modifier = Modifier
+                        .size(26.dp)
+                        .clip(CircleShape),
+                    alignment = Alignment.CenterStart,
+                    contentScale = ContentScale.Crop
+                )
+                Spacer(Modifier.padding(horizontal = 4.dp))
+                MyText(text = partido.visitante, fontSize = 24.sp)
+            }
+        }
+        Spacer(modifier = Modifier.weight(1f))
+        Box(
+            modifier = Modifier
+                .border(1.dp, Color.DarkGray, RoundedCornerShape(8.dp))
+        ) {
+            MyText(
+                text = partido.resultado,
+                textAlign = TextAlign.End,
+                modifier = Modifier.padding(16.dp, 4.dp)
+            )
+        }
+    }
+}
--- app/src/main/java/com/luisenrique/sportshub/ui/screens/MatchesScreen.kt
+++ app/src/main/java/com/luisenrique/sportshub/ui/screens/MatchesScreen.kt
@@ -1,2 +1,40 @@
 package com.luisenrique.sportshub.ui.screens
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.luisenrique.sportshub.ui.components.MatchItem
+
+data class Partido(
+    val fecha: String,
+    val local: String,
+    val visitante: String,
+    val resultado: String
+)
+
+@Composable
+fun MatchesScreen(modifier: Modifier) {
+    val partidos = listOf(
+        Partido("Sab 20:46", "fx.home", "fx.away", "  -  "),
+        Partido("Sab 20:47", "fx.home", "fx.away", "0 - 0"),
+        Partido("Sab 20:48", "fx.home", "fx.away", "  -  "),
+        Partido("Sab 20:49", "fx.home", "fx.away", "0 - 0"),
+        Partido("Sab 20:50", "fx.home", "fx.away", "  -  "),
+    )
+
+    LazyColumn(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        items(partidos) { partido ->
+            MatchItem(partido)
+            HorizontalDivider()
+        }
+    }
+}
