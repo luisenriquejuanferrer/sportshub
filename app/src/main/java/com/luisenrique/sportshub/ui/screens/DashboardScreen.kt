cf38dcb6f6dfe1b7b81bd64ea8481ed707585a42
chris <christian.abadmolina@gmail.com>
2025-11-21 15:38:32 +0100

    Borré import del DashboardScreen.kt xd

--- app/src/main/java/com/luisenrique/sportshub/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/luisenrique/sportshub/ui/screens/DashboardScreen.kt
@@ -1,10 +1,9 @@
 package com.ieschabas.sportshub.ui.screens
 
 
-import androidx.compose.foundation.background
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
