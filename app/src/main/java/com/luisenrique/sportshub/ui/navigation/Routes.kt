1bea99eef1d0c78114cce48e7c243a9877a31fd8
Luis Enrique <luijuafer@alu.edu.gva.es>
2025-11-26 20:56:17 +0100

    Creado constantes para rutas de navegacion

---
+++ app/src/main/java/com/luisenrique/sportshub/ui/navigation/Routes.kt
@@ -0,0 +1,17 @@
+package com.luisenrique.sportshub.ui.navigation
+
+object Routes {
+    const val LoginRegister = "login_register";
+    const val Register = "register";
+    const val Dashboard = "dashboard";
+    const val Leagues = "leagues";
+    const val LeagueDetail = "leagueDetail";
+    const val Teams = "teams";
+    const val TeamDetail = "teamDetail";
+    const val PlayerDetail = "playerDetail"
+    const val Matches = "matches";
+    const val MatchDetail = "matchDetail";
+    const val Clasification = "clasification";
+    const val Bets = "bets"
+    const val Profile = "profile";
+}
