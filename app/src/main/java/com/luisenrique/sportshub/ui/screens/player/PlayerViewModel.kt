ed3105e9fc3ec0c0180e31d2c561ea0f6ad3cb3a
chris <christian.abadmolina@gmail.com>
2026-01-21 17:11:47 +0100

    Creación de Clasification, Player y User viewModel

--- app/src/main/java/com/luisenrique/sportshub/ui/screens/player/PlayerViewModel.kt
+++ app/src/main/java/com/luisenrique/sportshub/ui/screens/player/PlayerViewModel.kt
@@ -1,9 +1,9 @@
-package com.luisenrique.sportshub.ui.screens.classification
+package com.luisenrique.sportshub.ui.screens.player
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.luisenrique.sportshub.domain.repository.ClassificationRepository
-import com.luisenrique.sportshub.domain.model.Classification
+import com.luisenrique.sportshub.domain.model.Player
+import com.luisenrique.sportshub.domain.repository.PlayerRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -11,12 +11,13 @@ import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class ClassificationViewModel @Inject constructor(
-    repository: ClassificationRepository
-) : ViewModel() {
-    val classifications: StateFlow<List<Classification>> =
-        repository.observeClassifications()
+class PlayerViewModel @Inject constructor(
+    repository: PlayerRepository
+): ViewModel() {
+    val players: StateFlow<List<Player>> =
+        repository.observePlayers()
             .stateIn(viewModelScope,
                 SharingStarted.WhileSubscribed(5_000),
                 emptyList())
+
 }
--- app/src/main/java/com/luisenrique/sportshub/ui/screens/user/UserViewModel.kt
+++ app/src/main/java/com/luisenrique/sportshub/ui/screens/user/UserViewModel.kt
@@ -1,9 +1,9 @@
-package com.luisenrique.sportshub.ui.screens.classification
+package com.luisenrique.sportshub.ui.screens.user
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.luisenrique.sportshub.domain.repository.ClassificationRepository
-import com.luisenrique.sportshub.domain.model.Classification
+import com.luisenrique.sportshub.domain.model.User
+import com.luisenrique.sportshub.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -11,12 +11,14 @@ import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
-class ClassificationViewModel @Inject constructor(
-    repository: ClassificationRepository
+class UserViewModel @Inject constructor(
+    repository: UserRepository
 ) : ViewModel() {
-    val classifications: StateFlow<List<Classification>> =
-        repository.observeClassifications()
+    val users: StateFlow<List<User>> =
+        repository.observeUsers()
             .stateIn(viewModelScope,
                 SharingStarted.WhileSubscribed(5_000),
                 emptyList())
+
+
 }
