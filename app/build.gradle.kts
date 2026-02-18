b897a9bbd4840845652e5cd3f204fd735f93b7ae
Luis Enrique <luijuafer@alu.edu.gva.es>
2026-02-18 11:12:26 +0100

    implementado libreria firestore para almacenar usuarios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.authentication)
+    implementation(libs.firebase.firestore)
 
     // --- TESTING ---
 
